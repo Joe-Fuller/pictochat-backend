bf40b31e9d05b00dd2f463c29c118bd0d3d71a20
Joe <joefuller042@gmail.com>
2023-07-25 11:53:57 +0100

    move cors up the middleware list

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
-const cors = require("cors")
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
+
 const server = http.createServer(app);
 const io = socketIO(server);
 
 const port = process.env.PORT || 3000;
-
-app.use(cors())
 
 io.on("connection", (socket) => {
   console.log("New user connected");
