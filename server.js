717a3d9697c0d6d55ef7045719977c10f474921f
Joe <joefuller042@gmail.com>
2023-07-25 11:10:29 +0100

    create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
