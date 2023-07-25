b52e3a0744376f6f95fbfe1451229e530b7eda6e
Joe <joefuller042@gmail.com>
2023-07-25 11:51:51 +0100

    use cors

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const http = require("http");
 const socketIO = require("socket.io");
+const cors = require("cors")
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
 const port = process.env.PORT || 3000;
+
+app.use(cors())
 
 io.on("connection", (socket) => {
   console.log("New user connected");
